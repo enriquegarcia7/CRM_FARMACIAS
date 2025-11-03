0c0d80dafa5abb5da44eb941701122c5920bdeba
acevedo86 <127603044+acevedo86@users.noreply.github.com>
2025-11-03 00:05:48 -0300

    arrglados errores de mege

--- Fase 2/Evidencias_Proyecto/sistema_aplicacion_V2/Frontend_React/src/App.jsx
+++ Fase 2/Evidencias_Proyecto/sistema_aplicacion_V2/Frontend_React/src/App.jsx
@@ -1,27 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/layout/Layout';
+import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Inventory from './pages/Inventory/Inventory';
 import Customers from './pages/Customers/Customers';
 import PurchaseSuggestions from './pages/PurchaseSuggestions/PurchaseSuggestions';
-import SeasonalDemand from './pages/SeasonalDemand/SeasonalDemand';  // ← NUEVO
 import OfertasLaboratorio from './pages/OfertasLaboratorio/OfertasLaboratorio';
 import ETL from './pages/ETL/ETL';
+import SeasonalDemand from './pages/SeasonalDemand/SeasonalDemand';
 
 function App() {
   return (
     <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="inventario" element={<Inventory />} />
-          <Route path="clientes" element={<Customers />} />
-          <Route path="demanda-estacional" element={<SeasonalDemand />} />  {/* ← NUEVO */}
-          <Route path="sugerencias" element={<PurchaseSuggestions />} />
-        </Route>
-      </Routes>
-
       <AuthProvider>
         <Routes>
           {/* Ruta pública de Login */}
@@ -39,10 +31,10 @@ function App() {
             <Route path="sugerencias" element={<PurchaseSuggestions />} />
             <Route path="ofertas-laboratorio" element={<OfertasLaboratorio />} />
             <Route path="etl" element={<ETL />} />
+            <Route path="demanda-estacional" element={<SeasonalDemand />} />
           </Route>
         </Routes>
       </AuthProvider>
-
     </BrowserRouter>
   );
 }
